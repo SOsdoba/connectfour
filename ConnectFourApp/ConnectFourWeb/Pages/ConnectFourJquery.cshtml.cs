df84c29ef8b00326d878b1b648a5c4bf133752b5
sosdoba <saraosdoba1@gmail.com>
2024-09-15 11:07:22 -0400

    HW 11

---
+++ ConnectFourApp/ConnectFourWeb/Pages/ConnectFourJquery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConnectFourWeb.Pages
+{
+    public class ConnectFourJqueryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
